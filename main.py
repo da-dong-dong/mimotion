89e4a5333d4df69a37d61a5982e8d172a1b8ad08
大冬冬 <40143949+da-dong-dong@users.noreply.github.com>
2025-09-29 10:41:05 +0800

    Update main.py
    固定最小步数

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def get_min_max_by_time(hour=None, minute=None):
     }
 
     min_step = 6000
-    max_step = 29885
+    max_step = get_int_value_default(config, "MAX_STEP", 24000)
     
     # 计算当前时间对应的步数范围（避免低于最小步数）
     current_min = max(min_step, int(time_rate * min_step))
